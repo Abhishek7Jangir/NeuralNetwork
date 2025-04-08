b41ad85e931176f7bcd58389498d3585446bb17a
Abhishek Jangir <abhishekjangir1234aj@gmail.com>
2025-04-09 00:44:23 +0530

    Add files via upload

---
+++ HandwrittenDigitRec.py
@@ -0,0 +1,23 @@
+import numpy as np
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = tf.keras.utils.normalize(X_train, axis=1)
+X_test = tf.keras.utils.normalize(X_test, axis=1)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=3)
+model.save('handwritten.keras')
+
+model = tf.keras.models.load_model('handwritten.keras')
+loss, accuracy = model.evaluate(X_test, y_test)
+print(loss, accuracy)
